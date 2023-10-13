6b4f58f8752d8ecb239b9a13ada462d0d0092cd7
wassimreg <wassim.regaieg@ede.ae>
2023-10-13 09:55:32 +0400

    more explicit output

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "api" {
-  value = "https://${aws_api_gateway_rest_api.pdf.id}-execute-api.${var.aws_region}.amazonaws.com/"
+  value = "https://${aws_api_gateway_rest_api.pdf.id}-execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.stage_name}"
 }
